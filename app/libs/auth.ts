025316d81f1ff5730cfc2fb642d4bcecf9c1ee25
Hak Yam <hakan@yaman.uk>
2024-05-23 16:02:24 +0300

    final

--- app/libs/auth.ts
+++ app/libs/auth.ts
@@ -1,5 +1,5 @@
 export async function login (email: string, password: string) {
-    const response = await fetch('http://localhost:3000/api/auth/login', {
+    const response = await fetch('/api/auth/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
